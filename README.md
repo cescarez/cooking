0bbb6864dd74169595227e4410869dbbce361589
Christabel Escarez <christabel.escarez@gmail.com>
2018-09-01 15:44:10 -0700

    add tsp to mL method

--- README.md
+++ README.md
@@ -1,18 +1,21 @@
-# _Calculator_
+# _Cooking App_
 
-#### _Epicodus Week 5, August 25, 2018_
+#### _Epicodus Week 5, September 1, 2018_
 
 #### By _**Christabel Escarez**_
 
 ## Description
 
-_Web-based calculator._
+_Web-based application to convert numbers to requested units._
 
 ## Setup/Installation Requirements
 
 * _Clone repository_
-* _Open index.html or webpage: https://cescarez.github.io/portfolio_
-* _Enter numbers and functions._
+* _Open cooking.html_
+* _Enter number to be converted._
+* _Enter starting unit._
+* _Enter desired unit._
+* _Run program
 
 ## Known Bugs
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,59 @@
-var add = function(number1, number2) {
-  return number1 + number2;
+var tspTomL = function (tablespoons) {
+  return tablespoons * 5
+};
+var tspToTbs = function (teaspoons) {
+  return teaspoons / 3
+};
+var tspToOz = function (teaspoons) {
+  return teaspoons / 6
+};
+var tspToCp = function (teaspoons) {
+  return teaspoons / 48
+};
+var tspToPints = function (tablespoons) {
+  return tablespoons / 96
+};
+var tspToQts = function (tablespoons) {
+  return tablespoons / 192
+};
+var tspToQts = function (tablespoons) {
+  return tablespoons / 768
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = add(number1, number2)
-alert(result);
+var tbsTomL = function (tablespoons) {
+  return tablespoons * 15
+};
+var tbsToTsp = function (tablespoons) {
+  return tablespoons * 3
+};
+var tbsToOz = function (tablespoons) {
+  return tablespoons / 2
+};
+var tbsToCp = function (tablespoons) {
+  return tablespoons / 16
+};
+var tbsToPints = function (tablespoons) {
+  return tablespoons / 32
+};
+var tbsToQts = function (tablespoons) {
+  return tablespoons / 64
+};
+var tbsToQts = function (tablespoons) {
+  return tablespoons / 256
+};
+
+var ozToTsp = function (ounces) {
+  return ounces * 6
+};
+
+var cpToTsp = function (cups) {
+  return cups*48
+};
+
+number = parseFloat(prompt("Enter a number: "))
+unitInit = prompt("Enter your starting unit of measurement: ")
+unitFinal = prompt("Enter your desired unit of measurement: ")
+
+
+result = tspToTbs(number)
+alert(result)
