3480488c14381b3ab59aaed4807161fb5ac417bd
Student <example@example.com>
2018-09-12 20:47:56 -0700

    begin conditional logic associated with drop down menu for tsp to any unit

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,36 @@
-var tspTomL = function (tablespoons) {
-  return tablespoons * 5
-};
-var tspToTbs = function (teaspoons) {
-  return teaspoons / 3
-};
-var tspToOz = function (teaspoons) {
-  return teaspoons / 6
-};
-var tspToCp = function (teaspoons) {
-  return teaspoons / 48
-};
+if ( "#tsp-units" === "milliliters") {
+  var tspTomL = function (tablespoons) {
+    return (tablespoons * 5);
+  };
+  var result = tspTomL(tspInput);
+} else if ("tablespoons") {
+  var tspToTbs = function (teaspoons) {
+    return teaspoons / 3
+  };
+} else if ("ounces") {
+  var tspToOz = function (teaspoons) {
+    return teaspoons / 6
+  };
+} else if ("cups") {
+  var tspToCp = function (teaspoons) {
+    return teaspoons / 48
+  };
+// } else {
+//   alert("We dont know what you mean. Try again.")
+// }
+
+// var tspTomL = function (tablespoons) {
+//   return tablespoons * 5
+// };
+// var tspToTbs = function (teaspoons) {
+//   return teaspoons / 3
+// };
+// var tspToOz = function (teaspoons) {
+//   return teaspoons / 6
+// };
+// var tspToCp = function (teaspoons) {
+//   return teaspoons / 48
+// };
 var tspToPints = function (tablespoons) {
   return tablespoons / 96
 };
@@ -50,10 +71,18 @@ var cpToTsp = function (cups) {
   return cups*48
 };
 
-number = parseFloat(prompt("Enter a number: "))
-unitInit = prompt("Enter your starting unit of measurement: ")
-unitFinal = prompt("Enter your desired unit of measurement: ")
 
-
-result = tspToTbs(number)
-alert(result)
+$(document).ready(function(){
+  $("form#tsp-to-ml").submit(function(event) {
+    event.preventDefault();
+    var tspInput = parseFloat($("#tsp-to-ml-input").val());
+    var result = tspTomL(tspInput);
+    $("#tsp-to-ml-output").text(tspInput + " teaspoons (tsp) to " + result + " millileters (mL)");
+  });
+  $("form#tsp-to-any").submit(function(event) {
+    event.preventDefault();
+    var tspToMlInput = parseFloat($("#tsp-to-any").val());
+    $("#tsp-to-any-output").text(tspToMlInput + " teaspoons (tsp) to " + result + " units");
+  });
+});
+};
